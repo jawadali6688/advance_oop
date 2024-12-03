e1664f47c1603f07d43282d336a0a7738333677e
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-12-03 22:31:11 +0500

    example of inheritance

--- oop_01.py
+++ oop_01.py
@@ -1,5 +1,7 @@
 class AddmissionForm():
 
+    merit_list = []
+
     def __init__(self, full_name, cnic, education):
         self.name = full_name
         self.cnic = cnic
@@ -8,22 +10,37 @@ class AddmissionForm():
     def show_user_data(self):
 
         return f"Full Name: {self.name} - CNIC: {self.cnic} - Education: {self.education}"
+    
+    
+    
+
+class MeritList(AddmissionForm):
+
+    def __init__(self, name):
+        print("Merit List")
+        self.name = name
+    
+    def add_into_merit_list(self):
+        super().merit_list.append(self.name)
 
 
-user_full_name = input("Your Name: ")
+# user_full_name = input("Your Name: ")
 
-user_cnic = input("Your CNIC: ")
+# user_cnic = input("Your CNIC: ")
 
-user_education = input("Your Education: ")
+# user_education = input("Your Education: ")
 
+# new_std = AddmissionForm(user_full_name, user_cnic, user_education)
 
-
-new_std = AddmissionForm(user_full_name, user_cnic, user_education)
-
-result = new_std.show_user_data()
+new_std = MeritList("Jawad")
 
 
-print(result)
+
+new_std.add_into_merit_list()
+
+
+
+print(new_std.merit_list)
 
 
 
